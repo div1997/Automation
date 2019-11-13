aac1f32e3b5d9c56cea152533398ec947289147f
div1997 <diya123verma@gmail.com>
2019-11-13 16:59:51 +0530

    policy pagination test case

--- src/main/java/pageobjects/Policy.java
+++ src/main/java/pageobjects/Policy.java
@@ -1,5 +1,6 @@
 package pageobjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -45,7 +46,7 @@ public class Policy {
 		WebElement editdoc;
 		@FindBy(xpath="//input[@id='exampleFormControlFile1']")
 		WebElement updatedoc;
-		@FindBy(xpath="//button[@class='btn btn-primary btn-section']")
+		@FindBy(xpath="/html/body/modal-container/div/div/div[2]/form/div[2]/button[2]")
 		WebElement saveupdate;
 		@FindBy(xpath="//option[@value='10']//parent::select")
 		WebElement showperpagefilter;
@@ -111,5 +112,92 @@ public WebElement saveupdate()
 public WebElement showperpage()
 {
 	return showperpagefilter;
+}
+
+@FindBy(xpath="//datatable-body//datatable-row-wrapper[1]//datatable-body-cell[3]//div[@class='datatable-body-cell-label']//span")
+WebElement filename;
+public WebElement filename()
+{
+	return filename;
+}
+//@FindBy(xpath="//ul[@class='pagination pull-right']//child::li//a[contains(text(),'2')]")
+//WebElement pagination;
+//public WebElement pagination()
+//{
+//	
+//	return pagination;
+//}
+public  void pagination_3()
+{
+//	for(int i=1; i<=2;i++)
+//	{
+//		
+//		WebElement pagination =driver.findElement(By.xpath("//ul[@class='pagination pull-right']//child::li//a[contains(text(),'i')]"));
+//		pagination.click();
+//	}
+	
+//	try catch
+	boolean p=true;
+	try
+	{
+		Boolean pagination =driver.findElement(By.xpath("//ul[@class='pagination pull-right']//child::li//a[contains(text(),'First')]//parent::li[@class='pagination-first page-item']")).isEnabled();
+	
+	}
+	catch(Exception e)
+	{
+		p=false;
+		
+	}
+	System.out.print(p);
+	if(p==true)
+	{
+		driver.findElement(By.xpath("//ul[@class='pagination pull-right']//child::li//a[contains(text(),'First')]")).click();
+		System.out.print("Click on frist page");
+	}
+	else
+	{
+		driver.findElement(By.xpath("//ul[@class='pagination pull-right']//child::li//a[contains(text(),'Last')]")).click();
+		System.out.print("Click on last page");
+	}
+}
+public void pagination_2()
+{
+	boolean p=true;
+	try
+	{
+		Boolean pagination =driver.findElement(By.xpath("//ul[@class='pagination pull-right']//child::li//a[contains(text(),'Previous')]//parent::li[@class='pagination-first page-item']")).isEnabled();
+	
+	}
+	catch(Exception e)
+	{
+		p=false;
+		
+	}
+//	Boolean pagination =driver.findElement(By.xpath("//ul[@class='pagination pull-right']//child::li//a[contains(text(),'Previous')]")).isEnabled();
+//	System.out.print(pagination);
+	if(p==true)
+	{
+		driver.findElement(By.xpath("//ul[@class='pagination pull-right']//child::li//a[contains(text(),'Previous')]")).click();
+		System.out.print("Click on frist page");
+	}
+	else
+	{
+		driver.findElement(By.xpath("//ul[@class='pagination pull-right']//child::li//a[contains(text(),'Next')]")).click();
+		System.out.print("Click on last page");
+	}
+}
+public void pagination_1()
+{
+	String totalpage= driver.findElement(By.xpath("//ul[@class='pagination pull-right']//child::li//a[contains(text(),'Last')]//parent::li//preceding-sibling::li[2]")).getText();
+	int i=Integer.parseInt(totalpage);  
+	for(int j=1;j<=i;j++)
+	{
+		String s =String.valueOf(j);  
+		WebElement pagevalue = driver.findElement(By.xpath("//ul[@class='pagination pull-right']//child::li//a[contains(text(),"+s+")]"));
+		pagevalue.click();
+		System.out.println(pagevalue);
+	}
+	
+	
 }
 }
--- src/test/java/OodlesDB/Policytestcase.java
+++ src/test/java/OodlesDB/Policytestcase.java
@@ -3,16 +3,18 @@ import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import junit.framework.Assert;
 import pageobjects.Landingpage;
 import pageobjects.Policy;
 import resource.Baseclass;
-import pageobjects.Commonelements;
-
+import resource.WaitStattementLib;
 public class Policytestcase extends Baseclass
 {
 
@@ -22,8 +24,6 @@ public class Policytestcase extends Baseclass
 		@BeforeTest
 		public void initialize() throws IOException  
 		{
-		
-		
 		driver = initializedriver();
 		driver.get(prop.getProperty("url"));
 		
@@ -37,8 +37,8 @@ public class Policytestcase extends Baseclass
 		Landingpage lp=new Landingpage(driver);
 		lp.login();
 		log.info("Login successfully");
-		Thread.sleep(2000);
-		
+		//Thread.sleep(2000);
+		WaitStattementLib.iWaitForSecs(driver, 20);
 		Policy pd = new Policy(driver);
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].click()", pd.Others());
@@ -52,14 +52,16 @@ public class Policytestcase extends Baseclass
 		pd.savetype().click();
 		log.info("save policy type");
 		
-		Thread.sleep(1000);
+		//Thread.sleep(1000);
+		WaitStattementLib.iWaitForSecs(driver, 10);
 		pd.adddocument().click();
 		log.info("Click on ad policy document");
 		Select option = new Select(pd.typedropdown());
 		option.selectByVisibleText("HR Manual");
 		log.info("option selected");
 		pd.uploadfile().sendKeys("/home/divya/Pictures/Screenshot from 2019-06-27 18-13-24.png");
-		Thread.sleep(1000);
+		//Thread.sleep(1000);
+		WaitStattementLib.iWaitForSecs(driver, 10);
 		//js.executeScript("arguments[0].click()", pd.savedoc());
 		log.info("document uploaded");
 		pd.savedoc().click();
@@ -67,37 +69,47 @@ public class Policytestcase extends Baseclass
 	
 	}
 	
-	@Test(enabled=true)
+	@Test(enabled=false)
 	public void editpolicydoc() throws InterruptedException
 	{
 		Landingpage lp=new Landingpage(driver);
 		lp.login();
-		Thread.sleep(2000);
-		
+		//Thread.sleep(2000);
+		WaitStattementLib.iWaitForSecs(driver, 20);
 		Policy pd = new Policy(driver);
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].click()", pd.Others());
 		js.executeScript("arguments[0].click()", pd.policy());
+		//Thread.sleep(2000);
+		WaitStattementLib.iWaitForSecs(driver, 20);
+		pd.search().sendKeys("HR Manual");
 		Thread.sleep(2000);
-		pd.search().sendKeys("HR");
 		log.info("applied serach filter");
 		pd.edit().click();
 		log.info("Click on edit icon");
-		pd.update().sendKeys("/home/divya/Pictures/ELK.png");
 		Thread.sleep(2000);
+		pd.update().sendKeys("/home/divya/Pictures/ELK_image.png");
+		Thread.sleep(5000);
+		//WaitStattementLib.iWaitForSecs(driver, 20);
 		pd.saveupdate().click();
 		log.info("update the document");
 		Thread.sleep(2000);
+		//WaitStattementLib.iWaitForSecs(driver, 50);
 		driver.navigate().refresh();
-		pd.search().sendKeys("HR");
+		pd.search().sendKeys("HR Manual");
+		Thread.sleep(2000);
+	String filename= "ELK_image.png";
+	String updatefilename= pd.filename().getText();
+	Assert.assertEquals(filename,updatefilename);
+	log.info("File updated successfully");
 	}
 	@Test(enabled=false)
 	public void showperpagefilter() throws InterruptedException
 	{
 		Landingpage lp=new Landingpage(driver);
 		lp.login();
-		Thread.sleep(2000);
-		
+		//Thread.sleep(2000);
+		WaitStattementLib.iWaitForSecs(driver, 20);
 		Policy pd = new Policy(driver);
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].click()", pd.Others());
@@ -105,5 +117,59 @@ public class Policytestcase extends Baseclass
 		Select option = new Select(pd.showperpage());
 		option.selectByVisibleText("Show 25 per page");
 		log.info("applied filter on show per page");
+	}
+	@Test(priority=1,enabled=false)
+	public void clickOnpagination_1() throws InterruptedException
+	{
+		Landingpage lp =new Landingpage(driver);
+		lp.login();
+		Thread.sleep(2000);
+		log.info("login successfully");
+		//WaitStattementLib.iWaitForSecs(driver, 20);
+		Policy pd = new Policy(driver);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click()", pd.Others());
+		log.info("click on Others tab");
+		js.executeScript("arguments[0].click()", pd.policy());
+		log.info("click on policy tab");
+		js.executeScript("window.scrollBy(0,1000)");
+		pd.pagination_1();
+		
+	}
+	@Test(priority=2,enabled=true)
+	public void clickOnpagination_2() throws InterruptedException
+	{
+		Landingpage lp =new Landingpage(driver);
+		lp.login();
+		Thread.sleep(2000);
+		log.info("login successfully");
+		//WaitStattementLib.iWaitForSecs(driver, 20);
+		Policy pd = new Policy(driver);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click()", pd.Others());
+		log.info("click on Others tab");
+		js.executeScript("arguments[0].click()", pd.policy());
+		log.info("click on policy tab");
+		js.executeScript("window.scrollBy(0,1000)");
+		pd.pagination_2();
+		
+	}
+	@Test(priority=3,enabled=false)
+	public void clickOnpagination_3() throws InterruptedException
+	{
+		Landingpage lp =new Landingpage(driver);
+		lp.login();
+		Thread.sleep(2000);
+		log.info("login successfully");
+//		//WaitStattementLib.iWaitForSecs(driver, 20);
+		Policy pd = new Policy(driver);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click()", pd.Others());
+		log.info("click on Others tab");
+		js.executeScript("arguments[0].click()", pd.policy());
+		log.info("click on policy tab");
+		js.executeScript("window.scrollBy(0,1000)");
+		pd.pagination_3();
+		
 	}
 }
