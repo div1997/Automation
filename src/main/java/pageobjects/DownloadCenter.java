5b7fe3d57bd034a915290dcd6782543ead2bd0cc
div1997 <diya123verma@gmail.com>
2019-11-14 15:13:44 +0530

    downloadcenter update the sequence of testcases

--- src/main/java/pageobjects/DownloadCenter.java
+++ src/main/java/pageobjects/DownloadCenter.java
@@ -42,8 +42,12 @@ public class DownloadCenter {
     
     @FindBy(xpath="//input[@name='fileTypeData2']")
     WebElement uploadfile;
-    @FindBy(xpath="//button[@class='btn btn-primary button-color customBtnCss']//parent::div//child::button[1]")
+    @FindBy(xpath="//form//div[3]//button[1][contains(text(),'Save')]")
     WebElement savedoc;
+    @FindBy(xpath="//thead//tr[2]//th[5]//input[@type='text']")
+    WebElement searchDocumentype;
+    @FindBy(xpath="//tbody//tr[1]//td[6]//span[2]//a[1]//span[@class='fas fa-pencil-alt icon-color']")
+    WebElement Edit;
 	public WebElement project()
 	{
 		return project;
@@ -76,6 +80,31 @@ public class DownloadCenter {
 	{
 		return save;
 	}
+	
+	@FindBy(xpath="//tbody//tr[2]//td[4]//i[1][@class='fas fa-pencil-alt icon-color']")
+	WebElement editdoctypebutton;
+	public WebElement editdoctypebutton()
+	{
+		return editdoctypebutton;
+	}
+	@FindBy(xpath="//form//div//div//div[@class='row']//div//div//input[@id='formGroupExampleInput1']")
+	WebElement editdoctypename;
+	public WebElement editdoctypename()
+		{
+			return editdoctypename;
+		}
+	@FindBy(xpath="//form//div//div//div[@class='row']//div//div[2]//input[@id='formGroupExampleInput2']")
+	WebElement editdoctypedescription;
+	public WebElement editdoctypedescription()
+	{
+		return editdoctypedescription;
+	}
+	@FindBy(xpath="//form//div[2]//button[2][@class='btn btn-primary btn-section']")
+	WebElement saveeditdoctype;
+	public WebElement saveEditDocType()
+	{
+		return saveeditdoctype;
+	}
 	public WebElement close()
 	{
 		return close;
@@ -98,4 +127,48 @@ public class DownloadCenter {
 		return savedoc;
 	}
 	
+	public WebElement searchDocumentype ()
+	{
+		return searchDocumentype;
+	}
+	public WebElement edit() 
+	{
+		return Edit;
+	}
+	@FindBy(xpath="//form//div[1]//div//div//div[2]//div[2]//input[@id='exampleFormControlFile1']")
+	WebElement updatedDoc;
+	public WebElement updatedDoc() {
+		return updatedDoc;
+	}
+	@FindBy(xpath="//form//div[2]//button[2][@class='btn btn-primary btn-section']")
+	WebElement saveEdit;
+	public WebElement saveEdit()
+	{
+		return saveEdit;
+	}
+	@FindBy(xpath="//tbody//tr[1]//td[6]//span[2]//a[2]//span[@class='fas fa-download icon-color']")
+	WebElement download;
+	public WebElement download()
+	{
+		return download;
+	}
+	@FindBy(xpath="//tbody//tr[1]//td[6]//span[2]//a[3]//span[@class='fas fa-trash-alt icon-color']")
+	WebElement delete;
+	public WebElement delete(){
+		{
+			return delete;
+		}
+	}
+	@FindBy(xpath="//div//form//div//div//div//button[1][@class='btn btn-primary']")
+	WebElement deleteyes;
+	public WebElement deleteyes()
+	{
+		return deleteyes;
+	}
+@FindBy(xpath="//select[@class='form-control customSelectCss ng-pristine ng-valid ng-touched']")
+WebElement showperpage;
+public WebElement showperpage() 
+{
+	return showperpage;
+}
 }
--- src/test/java/OodlesDB/Downloadcentertestcase.java
+++ src/test/java/OodlesDB/Downloadcentertestcase.java
@@ -2,8 +2,12 @@ package OodlesDB;
 
 import java.io.IOException;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -11,53 +15,142 @@ import org.testng.annotations.Test;
 import pageobjects.DownloadCenter;
 import pageobjects.Landingpage;
 import resource.Baseclass;
-import pageobjects.Commonelements;
 public class Downloadcentertestcase extends Baseclass {
 	
 	
 	
 	WebDriver driver;
 	
+	
+	DownloadCenter dc;
 	@BeforeTest
 	public void initialize() throws IOException  
 	{
-	
-	
 	driver = initializedriver();
 	driver.get(prop.getProperty("url"));
 	
 	}
+	
+	
 	@Test
-	public void adddocumenttype() throws InterruptedException
+	public void login() throws InterruptedException
 	{
 		Landingpage lp=new Landingpage(driver);
 		lp.login();
-		Thread.sleep(2000);
 		
-		DownloadCenter dc = new DownloadCenter(driver);
+		Thread.sleep(2000);
+	}
+	
+	@Test (dependsOnMethods={"login"})
+	public void downloadpageNavigation()
+	{
+	     dc = new DownloadCenter(driver);
 		JavascriptExecutor js = (JavascriptExecutor)driver;
-		js.executeScript("arguments[0].click()", dc.project());
-		js.executeScript("arguments[0].click()", dc.downloadcenter());
-	
-	dc.addtype_button1().click();
-	Thread.sleep(1000);
-	dc.add_button().click();
-//	dc.doctypeName().sendKeys("Project RTM3");
-//	dc.doctypedescription().sendKeys("Complete requirement tracibilty document");
-//	Thread.sleep(1000);
-//	dc.save().click();
-//	dc.close().click();
-	/*Thread.sleep(1000);
-	dc.adddocument().click();
-	
-	Select option = new Select (dc.dropdown());
-	option.deselectByVisibleText("Project RTM");
-	dc.upload().sendKeys("/home/divya/Pictures");
-//	dc.savedoc().click();
-	*/
+		
+		js.executeScript("arguments[0].click();", dc.project());
+		js.executeScript("arguments[0].click();", dc.downloadcenter());
 		
 	}
 	
 	
+	@Test(dependsOnMethods={"downloadpageNavigation"},enabled=false)
+	public void addDocumentype_downloadcenter() throws InterruptedException
+	{
+	
+	dc.addtype_button1().click();
+	Thread.sleep(2000);
+	dc.add_button().click();
+	dc.doctypeName().sendKeys("Project RTM_23");
+	dc.doctypedescription().sendKeys("Complete requirement tracibilty document");
+    Thread.sleep(2000);
+	dc.save().click();
+	Thread.sleep(2000);
+	dc.close().click();
+	Thread.sleep(1000);
+		
+	}
+	
+	
+	@Test(dependsOnMethods={"downloadpageNavigation"},enabled=false)
+	public void editDocumenttype() throws InterruptedException
+	{
+		dc.addtype_button1().click();
+		Thread.sleep(2000);
+		dc.editdoctypebutton().click();
+		dc.editdoctypename().sendKeys(Keys.CONTROL + "a");
+		dc.editdoctypename().sendKeys(Keys.DELETE);
+		dc.editdoctypename().sendKeys("rrr");
+		dc.editdoctypedescription().sendKeys(Keys.CONTROL + "a");
+		dc.editdoctypedescription().sendKeys(Keys.DELETE);
+		dc.editdoctypedescription().sendKeys("jhdkjhdjkhskjdh");
+		dc.saveEditDocType().click();
+		Thread.sleep(2000);
+		dc.close().click();
+	}
+	
+	
+	@Test(dependsOnMethods={"downloadpageNavigation"},enabled=false)
+	public void addDocumentDownloadcenter() throws InterruptedException
+	{
+	dc.adddocument().click();
+	
+	Select option = new Select (dc.dropdown());
+	option.selectByVisibleText("Project RTM_23");
+	dc.upload().sendKeys("/home/divya/Pictures/ELK.png");
+	Thread.sleep(1000);
+	dc.savedoc().click();
+	}
+	
+	@Test(dependsOnMethods={"downloadpageNavigation"},enabled=false)
+	public void searchbyDocumentType() throws InterruptedException
+	{
+	dc.searchDocumentype().sendKeys("Project RTM_23");
+	Thread.sleep(2000);
+	dc.searchDocumentype().sendKeys(Keys.CONTROL + "a");
+	dc.searchDocumentype().sendKeys(Keys.DELETE);
+//	
+//	dc.searchDocumentype().clear();
+//	dc.searchDocumentype().click();
+//	
+//	
+    }
+	
+	@Test(dependsOnMethods={"downloadpageNavigation"},enabled=false)
+	public void downloadDocument() throws InterruptedException 
+	{
+		dc.searchDocumentype().sendKeys("Project RTM_23");
+		Thread.sleep(2000);
+		dc.download().click();
+	}
+	
+	
+	@Test(dependsOnMethods={"downloadpageNavigation"},enabled=true)
+	public void deleteDocument() throws InterruptedException
+	{
+		dc.searchDocumentype().sendKeys("Project RTM_23");
+		Thread.sleep(2000);
+		dc.delete().click();
+//		 Alert alert = driver.switchTo().alert();
+//		 alert.accept();
+		dc.deleteyes().click();
+		dc.searchDocumentype().sendKeys(Keys.CONTROL + "a");
+		dc.searchDocumentype().sendKeys(Keys.DELETE);
+	}
+	
+	
+	@Test(dependsOnMethods={"downloadpageNavigation"},enabled=false)
+	public void showPerPageFilter() 
+	{
+		
+	//Select option = new Select(dc.showperpage());
+	//option.selectByVisibleText("Show 50 per page");
+	
+		}
+	
+	@Test(enabled=false)
+	public void pagination()
+	{
+		
+	}
 
 }
